632cf5b3b5f6c0b324f27fef2926934e6904d342
Kris Manohar <justkrismanohar@gmail.com>
2016-12-03 23:07:23 -0400

    fixed install.sh

--- install.sh
+++ install.sh
@@ -11,5 +11,6 @@ sudo ln -s /usr/bin/nodejs /usr/bin/node
 git clone https://github.com/Snickdx/comp6905a3.git
 cd comp6905a3
 sudo npm install
+sudo npm install forever -g
 sudo forever start ''$workserver_path'/comp6905a3/sender.js'
 sudo forever list
